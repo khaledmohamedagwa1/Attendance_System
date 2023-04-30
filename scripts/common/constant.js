923faaab8f111b2d7bae596af6a68481a0c8a497
Khaled Mohamed <131799217+khaledmohamedagwa1@users.noreply.github.com>
2023-05-01 00:03:12 +0200

    Update constant.js

--- scripts/common/constant.js
+++ scripts/common/constant.js
@@ -1,7 +1,7 @@
 const dashboardURL = "admin/dashboard.html";
 const homeEmployeeURL = "home.html";
 const homeSecurityManURL = "seurtity.html";
-const loginUrl = "login.html";
+const loginUrl = "index.html";
 const apiKey =
   "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";
 const startTime = new Date();
