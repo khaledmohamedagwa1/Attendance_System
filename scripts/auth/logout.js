c761dfb7183540db0ac3b6667376cb00526f6dfd
Khaled Mohamed <131799217+khaledmohamedagwa1@users.noreply.github.com>
2023-05-18 17:58:10 +0300

    Update logout.js

--- scripts/auth/logout.js
+++ scripts/auth/logout.js
@@ -1,9 +1,18 @@
 import * as session from "./../common/session.js";
 import * as constant from "./../common/constant.js";
 let currentUser = null;
+
 let logout = document.getElementById("logout");
+let currentURl = location.href;
+let urlParts = currentURL.split("/");
+
 logout.addEventListener("click", function () {
-  console.log("logOut Found");
+if(urlParts[length - 2] == "admin"])
+{
+let loginUrl = urlParts.slice(0,-2).join("/");
+location.replace(loginUrl);
+return;
+}
   session.setSession(currentUser);
   location.replace("index.html");
 });
